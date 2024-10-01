7a464c68d2d8728c394e4d575f37213ab9c47f38
mv-rom <roman.mareev@gmail.com>
2024-10-01 13:54:13 +0300

    some elements of deployment

--- compile.bat
+++ compile.bat
@@ -1,9 +1,10 @@
 @echo off
 
 rem set name=%~n0
-rem set dir=%~dp0
+set ProjectDirPath=%~dp0
 set ProjectName=AE
 set DotnetPath=C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319
+set RefAssemPath=%ProjectDirPath%\libframework
 
 rem dotnet build --configuration Release --arch win-x64
 
@@ -11,6 +12,6 @@ rem https://learn.microsoft.com/en-us/visualstudio/msbuild/msbuild-command-line-
 rem http://docwiki.embarcadero.com/RADStudio/Alexandria/en/Building_a_Project_Using_an_MSBuild_Command
 
 
-%DotnetPath%\MSBuild.exe %ProjectName%.sln /nologo /t:Rebuild /p:Configuration=Release;FrameworkPathOverride=%DotnetPath%
+%DotnetPath%\MSBuild.exe %ProjectName%.sln /nologo /t:Rebuild /p:Configuration=Release;FrameworkPathOverride=%RefAssemPath%
 
 pause
--- postbuild.bat
+++ postbuild.bat
@@ -1,3 +1,6 @@
 @echo off
 
+echo %1
+echo %2
 copy /V /Y %1\..\..\ae.jsonconfig %1
+copy /V /Y %2\libframework\*.* %1\libs
