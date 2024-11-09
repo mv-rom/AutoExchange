2f1ed1f1f045b9aea1b472ba88c416af1d193414
mv-rom <roman.mareev@gmail.com>
2024-11-09 14:25:22 +0200

    changing

--- src/service_releases/VchasnoEDI/Properties/AssemblyInfo.cs
+++ src/service_releases/VchasnoEDI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyTitle("VchasnoEDI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyProduct("VchasnoEDI")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9cd557af-4a86-4b7e-96c1-9032fe33e978")]
+[assembly: Guid("8b4fe48f-8b1e-48c3-8351-30a3bde61f38")]
 
 // Version information for an assembly consists of the following four values:
 //
