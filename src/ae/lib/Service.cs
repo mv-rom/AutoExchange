8f4c6000bc26dee752a0a0b0ebca91de3f6215c4
mv-rom <roman.mareev@gmail.com>
2024-11-28 22:14:50 +0200

    +

--- src/ae/lib/Service.cs
+++ src/ae/lib/Service.cs
@@ -41,41 +41,8 @@ namespace ae.lib
             return true;
         }
 
-        public bool Run()
+        public void Run(string Name)
         {
-            bool result = false;
-
-            // проверка на то, есть ли загружаемая сборка среди наших статических ссылок
-            // взята из https://support.microsoft.com/ru-ru/kb/837908
-            // но вам может быть и не нужна, подумайте сами
-            if (!Assembly.GetExecutingAssembly().GetReferencedAssemblies().Any(
-                asmName => asmName.FullName.Substring(0, asmName.FullName.IndexOf(",")) == "Name")
-            )
-            {
-                return false;
-            }
-
-
-            /* 
-                foreach (Type type in DLL.GetExportedTypes())
-                {
-                    var c = Activator.CreateInstance(type);
-                    type.InvokeMember("Output", BindingFlags.InvokeMethod, null, c, new object[] { @"Hello" });
-                }
-            */
-            /*
-                foreach (Type type in DLL.GetExportedTypes())
-                {
-                    dynamic c = Activator.CreateInstance(type);
-                    c.Output(@"Hello");
-                }
-            */
-            var theType = DLL.GetType("DLL.ActionReleaseClass");
-            var c = Activator.CreateInstance(theType);
-            var method = theType.GetMethod("Start");
-            method.Invoke(c, new object[] { @"" }); //instance to Base
-            result = true;
-            return result;
         }
     }
 }
