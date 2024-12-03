234f27c7d6b687069c4e4eea214aaa269bc1b24e
mv-rom <roman.mareev@gmail.com>
2024-12-03 03:28:27 +0200

    +

--- src/ae/services/EDI/EDI.cs
+++ src/ae/services/EDI/EDI.cs
@@ -678,8 +678,6 @@ namespace ae.services.EDI
                         if (SplittedOrders == null)
                             throw new Exception("Result of doSplittingUpOrders is null.");
 
-                        throw new Exception("STOP");
-
                         var combineStatus = CombineAbiePreSalesAndOrders(ref SplittedOrders);
 
                         var checkStatus1c = CheckAndAddOrdersIn1C(ref SplittedOrders);
